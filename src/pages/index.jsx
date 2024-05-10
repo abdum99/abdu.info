7b5e648997b6ec9f5c29c165f4efba2e86ddb565
amohamdy99 <a.mohamdy99@gmail.com>
2024-05-10 14:09:45 -0700

    minor: fixed header centering

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -283,6 +283,57 @@ function Photos() {
   )
 }
 
+function Intro() {
+  return (
+    <Container className="mt-9">
+      <div className='flex flex-row-reverse'>
+        <div className='flex-initial basis-1/3 min-w-80 m-6'>
+          <Image
+            src={avatarImage}
+            alt=""
+            className={clsx(
+              'aspect-square rounded-3xl bg-zinc-100 object-cover dark:bg-zinc-800 w-80'
+            )}
+          />
+        </div>
+        <div className="max-w-4xl basis-2/3 mt-8">
+          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+            ABDU MOHAMDY
+          </h1>
+          <h2 className="text-2xl font-thin tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+            Software Engineer
+          </h2>
+          <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+            I'm Abdu, a software engineer based out of San Francisco. I love research and hacking and I'm passtionate about education accessibility and freedom of the press. I enjoy reading, cooking, and building objectively cool stuff in my free time.
+          </p>
+          <div className="mt-6 flex gap-6">
+            <SocialLink
+              href="https://twitter.com"
+              aria-label="Follow on Twitter"
+              icon={TwitterIcon}
+            />
+            <SocialLink
+              href="https://instagram.com"
+              aria-label="Follow on Instagram"
+              icon={InstagramIcon}
+            />
+            <SocialLink
+              href="https://github.com"
+              aria-label="Follow on GitHub"
+              icon={GitHubIcon}
+            />
+            <SocialLink
+              href="https://linkedin.com"
+              aria-label="Follow on LinkedIn"
+              icon={LinkedInIcon}
+            />
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+}
+
 export default function Home({ articles }) {
   return (
     <>
@@ -295,52 +346,7 @@ export default function Home({ articles }) {
           content="I'm Abdu, a software engineer based out of San Francisco. I love research and hacking and I'm passtionate about education accessibility and freedom of the press. I enjoy reading, cooking, and building objectively cool stuff in my free time"
         />
       </Head>
-      <Container className="mt-9">
-        <div className='flex flex-row-reverse'>
-          <div className='flex-initial basis-1/3 min-w-80 m-6'>
-            <Image
-              src={avatarImage}
-              alt=""
-              className={clsx(
-                'aspect-square rounded-3xl bg-zinc-100 object-cover dark:bg-zinc-800 w-80'
-              )}
-            />
-          </div>
-          <div className="max-w-4xl basis-2/3">
-            <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              ABDU MOHAMDY
-            </h1>
-            <h2 className="text-2xl font-thin tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              Software Engineer & Tech Enthusiast.
-            </h2>
-            <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-              I'm Abdu, a software engineer based out of San Francisco. I love research and hacking and I'm passtionate about education accessibility and freedom of the press. I enjoy reading, cooking, and building objectively cool stuff in my free time.
-            </p>
-            <div className="mt-6 flex gap-6">
-              <SocialLink
-                href="https://twitter.com"
-                aria-label="Follow on Twitter"
-                icon={TwitterIcon}
-              />
-              <SocialLink
-                href="https://instagram.com"
-                aria-label="Follow on Instagram"
-                icon={InstagramIcon}
-              />
-              <SocialLink
-                href="https://github.com"
-                aria-label="Follow on GitHub"
-                icon={GitHubIcon}
-              />
-              <SocialLink
-                href="https://linkedin.com"
-                aria-label="Follow on LinkedIn"
-                icon={LinkedInIcon}
-              />
-            </div>
-          </div>
-        </div>
-      </Container>
+      <Intro />
       <Photos />
       <Container className="mt-24 md:mt-28">
         <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
