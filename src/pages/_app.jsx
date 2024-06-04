2e4cca249caf285918170daeb1b03124a8d1b883
amohamdy99 <a.mohamdy99@gmail.com>
2024-06-03 23:33:40 -0700

    fixed header scroll issue

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -40,7 +40,7 @@ export default function App({ Component, pageProps, router }) {
           <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-0" />
         </div>
       </div>
-      <div className="relative overflow-x-hidden">
+      <div className="relative">
         <Header />
         <main>
           <Component previousPathname={previousPathname} {...pageProps} />
