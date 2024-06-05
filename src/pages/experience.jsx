fae0062eb902388c2db3705e9a75be8e6581bdbf
amohamdy99 <a.mohamdy99@gmail.com>
2024-06-04 22:49:54 -0700

    made roles on index links

--- src/pages/experience.jsx
+++ src/pages/experience.jsx
@@ -47,7 +47,7 @@ function Roles({ props }) {
         <div class="-my-6">
             {roles.map((role) => (
                 <div key={role.slug} id={role.slug} class="relative pl-8 sm:pl-32 py-6 group">
-                    <div class="flex flex-col sm:flex-row items-start group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-zinc-200 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-2 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-zinc-500 dark:after:bg-zinc-50 after:border-8 after:box-content after:border-zinc-50 dark:after:border-zinc-900 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+                    <div class="flex flex-col sm:flex-row items-start group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-zinc-200 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-2 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-zinc-500 dark:after:bg-zinc-50 after:border-8 after:box-content after:border-white dark:after:border-zinc-900 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
                         <motion.div className="sm:absolute left-9 text-xs font-semibold uppercase w-12 bg-zinc-100 rounded-full flex flex-none shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0"
                         initial="offscreen"
                         whileInView="onscreen"
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -242,15 +242,18 @@ function Work() {
       viewport={{ once: true, amount: 0.3 }}
       variants={fadeInTopMotionVariant}
     >
-    <div className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
+    <div className="rounded-2xl border border-zinc-100 pt-6 px-3 dark:border-zinc-700/40">
+      <div className='px-2'>
       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
         <BriefcaseIcon className="h-6 w-6 flex-none" />
         <span className="ml-3">Work</span>
       </h2>
-      <ol className="mt-6 space-y-4">
+      </div>
+      <ol className="mt-3 space-y-0.5">
         {roles.map((role, roleIndex) => (
-          <li key={roleIndex} className="flex gap-4">
-            <div className="relative mt-5 p-0.5 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+          <li key={roleIndex} >
+            <Link href={`/experience#${role.slug}`} className="flex gap-3 py-4 px-3 rounded-xl hover:bg-zinc-50">
+            <div className="relative mt-1 p-0.5 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
               <Image src={role.logo} alt="" className="w-20 rounded-full" unoptimized />
             </div>
             <dl className="flex flex-auto flex-wrap gap-x-2">
@@ -278,6 +281,7 @@ function Work() {
                 </time>
               </dd>
             </dl>
+            </Link>
           </li>
         ))}
       </ol>
